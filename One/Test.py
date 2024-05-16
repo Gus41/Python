057001fcbf43f4264cc3a98b819eea22a9aa6a2e
Gustavo <123191793+Gus41@users.noreply.github.com>
2024-05-16 09:10:05 -0300

    Update Test.py

--- One/Test.py
+++ One/Test.py
@@ -1,1 +1,9 @@
-print("Hello World")
+class Pessoa:
+    def __init__(self,name):
+        self.name = name
+
+    def saiHy(self):
+        print(f'{self.name} is saing hy!')
+
+p1 = Pessoa("Teste")
+p1.saiHy()
