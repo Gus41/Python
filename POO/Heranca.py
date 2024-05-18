f5f6bd01b75eae3315010449c367494968feca15
Gustavo <123191793+Gus41@users.noreply.github.com>
2024-05-18 15:31:01 -0300

    Update Heranca.py

--- POO/Heranca.py
+++ POO/Heranca.py
@@ -1,3 +1,14 @@
-class Foo:
-    ...
+class Pessoa:
+    def __init__(self,name) -> None:
+        self.name = name
 
+    def saiHy(self):
+        print(self.__class__)
+
+class Cliente(Pessoa):
+    def __init__(self, name) -> None:
+        super().__init__(name)
+
+class Aluno(Pessoa):
+    def __init__(self, name) -> None:
+        super().__init__(name)
