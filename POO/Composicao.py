68ce95a3536dfed6434216966fc2622cd87b386a
Gustavo <123191793+Gus41@users.noreply.github.com>
2024-05-18 15:21:00 -0300

    Update Composicao.py

--- POO/Composicao.py
+++ POO/Composicao.py
@@ -11,5 +11,46 @@ class Client:
 
     def insertEnderecos(self,rua,numero):
         self.enderecos.append(Endereco(rua,numero))
+    
+    def listarEnderecos(self):
+        for end in self.enderecos:
+            print(end.rua)
 
+
+Andre = Client("Andre")
+Andre.insertEnderecos("Br-116",123)
+Andre.listarEnderecos()
 #Quando o objeto Cliente deixar de dexistir, os endereços também irão
+class Motor:
+    
+    def __init__(self,nome) -> None:
+        self.nome = nome
+    
+
+class Fabricante:
+    
+    def __init__(self,name):
+        self.name = name
+        self._carros = []
+    
+    def inserNewCar(self,car):
+        self.carros.append(car)
+
+class Car:
+    _motor : Motor
+    fabircante : Fabricante
+
+    def __init__(self,name):
+        self.name = name
+        self._motor = None
+        self._fabircante = None
+
+    @property
+    def motor(self):
+        return self._motor
+
+    @motor.setter
+    def motor(self,motor):
+        self._motor = motor    
+
+    
