d935ddcd0079d99e157e4aa27e1c068020f7b32f
Gustavo <123191793+Gus41@users.noreply.github.com>
2024-05-20 07:25:13 -0300

    .

--- POO/Mixing/log.py
+++ POO/Mixing/log.py
@@ -1,12 +1,20 @@
 #Abstração
 class Log:
-    def log(self,msg):
+    def _log(self,msg):
         raise NotImplemented("Implemente o metodo Log")
+    
+    def LogError(self,msg):
+        self._log(f'Error : {msg}')
+
+    def logSucces(self,msg):
+        self._log(f'Succes: {msg}')
+
 
 class LogFileMixin(Log):
-    ...
+    def _log(self, msg):
+        print(msg)
 
  
-if __name__ == 'main':
-    l = Log()
-    l.log()
+if __name__ == '__main__':
+    l = LogFileMixin()
+    l.LogError("Testing")
