af4a209c3a7df1fa90cbc5f7746d0c8d76ed79f2
Gustavo <123191793+Gus41@users.noreply.github.com>
2024-05-26 20:50:34 -0300

    Create Abc.py

---
+++ POO/Abc.py
@@ -0,0 +1,25 @@
+from abc import ABC,abstractmethod
+
+
+
+class Notification(ABC):
+    def __init__(self,mensagem) -> None :
+        self._mensagem = mensagem
+    
+    @abstractmethod
+    def send(self) -> bool:...
+
+class Sms(Notification):
+    def send(self) -> True: 
+        print(self._mensagem)
+        return True
+
+def notificar(mensagem:Notification):
+    enviada = mensagem.send()
+
+    if enviada :
+        print("Notificação enviada")
+    else : 
+        print("Falha em enviar mensagem")
+
+notificar(Sms("Testando"))
