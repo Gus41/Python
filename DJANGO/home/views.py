7983585cccf46c66c241dd85e7617d767bb2840b
Gustavo <123191793+Gus41@users.noreply.github.com>
2024-06-17 07:23:10 -0300

    .

--- DJANGO/home/views.py
+++ DJANGO/home/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from home import data
+
 
 
 # Create your views here.
 def home(request):
     print("home")
-    context = { 'text' : 'Texto passado por parametro'}
+    context = { 'tittle' : 'Home',
+                'posts' : data.posts}
     return render(request,'home.html',context)
